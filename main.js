d79951721635e48191fec18cad5857ee21d5466e
TsimurZhylinski <zhilinskiyt@gmail.com>
2020-11-13 19:34:42 +0300

    feat : finish drum kit project

---
+++ main.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!audio) return; //stop the function
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+});
+
+function removeTrasition(e) {
+    if (e.propertyName !== 'transform') return; // skip it if it not transform
+    this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTrasition));  
+
